f3ef4246b829b59330b8f2cd0b2b44e0ac2f8563
Ajaz7267 <ajaz726@gmail.com>
2023-01-20 14:42:21 +0530

    EvenOrOdd

---
+++ EvenOrOdd.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+
+
+
+    public class EvenOrOdd {
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter The Number:");
+            int i= sc.nextInt();
+            if(i%2==0){
+                System.out.println("This Number is Even");
+            }else if(i%2==1){
+                System.out.println("This Number is Odd");
+            }
+        }
+    }
+
+
